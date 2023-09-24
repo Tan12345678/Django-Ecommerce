c2d31cd68e89f98230f77b734117b21e28763e31
Tanmoy Malakar <tanmoymalakar@Tanmoys-MacBook-Air.local>
2023-09-24 23:07:58 +0530

    deploying to Railway

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yh46pf4qog1g!)aslza3osyd9*=9bdegh=01wjn9%yq-cse&^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT='/static/'
+
+STATIC_ROOT="/Users/tanmoymalakar/PycharmProjects/E-commerce-website-in django/E_shop/collected_static"
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
